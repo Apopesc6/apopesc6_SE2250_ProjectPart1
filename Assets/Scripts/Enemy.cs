acd120e19a2432a377e5a39d4226b78623db902a
Apopesc6 <35278433+Apopesc6@users.noreply.github.com>
2018-02-22 16:41:09 -0500

    Finished Project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    public float xSpeed = 10f;
+
+    Vector3 tempPos;
+    Vector3 tempPos1;
+    private float randomNumber;
+    private bool runOnce = true;
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        randomNumber = Random.Range(1,2);
+        for (int i = 0; i < randomNumber; i++)
+        {
+            xSpeed = xSpeed * -1;
+        }
+        
+
+	}
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if(bndCheck != null && bndCheck.offDown)
+        {
+            Destroy(gameObject);
+        }
+        if (bndCheck != null && (bndCheck.offRight || bndCheck.offLeft))
+        {
+            if(runOnce)
+            {
+                ChangeXDirection();
+                runOnce = false;
+            }
+        }
+    }
+
+    void ChangeXDirection()
+    {
+        xSpeed = xSpeed * -1;
+    }
+
+    public virtual void Move()
+    {
+        if(gameObject.CompareTag("enemy0"))
+        {
+            tempPos = pos;
+            tempPos.y -= speed * Time.deltaTime;
+            pos = tempPos;
+        }
+        if (gameObject.CompareTag("enemy1"))
+        {
+            tempPos1 = pos;
+            tempPos1.y -= speed * Time.deltaTime;
+            tempPos1.x -= xSpeed * Time.deltaTime;
+            pos = tempPos1;
+        }
+
+    }
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -15,14 +15,31 @@ public class Main : MonoBehaviour {
     {
         S = this;
         bndCheck = GetComponent<BoundsCheck>();
-        
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
     }
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-    
+    public void SpawnEnemy()
+    {
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        float enemyPadding = enemyDefaultPadding;
+        if(go.GetComponent<BoundsCheck>() != null)
+        {
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
 	// Update is called once per frame
 	void Update () {
 		
