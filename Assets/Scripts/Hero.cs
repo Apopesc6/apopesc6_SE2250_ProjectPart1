1b6a3556c749b3bb942b6a97e2d5cef4289b23b2
Apopesc6 <35278433+Apopesc6@users.noreply.github.com>
2018-02-22 16:35:56 -0500

    Initial commit

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+    public float shipSpeed = 40f;
+    public float rollMult = -45f;
+    public float pitchMult = 30f;
+
+    // Use this for initialization
+    void Start() {
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * shipSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * shipSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * shipSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * shipSpeed * Time.deltaTime;
+        }
+
+        float xAxis = Input.GetAxis("horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * shipSpeed * Time.deltaTime;
+        pos.y += yAxis * shipSpeed * Time.deltaTime;
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+       
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+    static public Main S;
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 1f;
+    public float enemyDefaultPadding = 1.5f;
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        S = this;
+        bndCheck = GetComponent<BoundsCheck>();
+        
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
